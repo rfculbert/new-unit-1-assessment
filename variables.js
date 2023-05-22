8e214ac537abbd4e2f9d3bebc9db1457e2353314
Robert Culbert <bertbert@Roberts-MBP.attlocal.net>
2023-05-22 13:20:32 -0500

    final commit

--- variables.js
+++ variables.js
@@ -2,17 +2,20 @@
 /* Create a variable called 'myName' with a value that is your name as a string. */
 
 //CODE HERE 
-
+console.log("---q1----")
+let myName = "Robert"
 //////////////////PROBLEM 2////////////////////
 /* Create a variable called 'faveNum' with a value that is your favorite number. */
 
 //CODE HERE
-
+console.log("---q2---")
+let favNum = 6;
 //////////////////PROBLEM 3////////////////////
 /* Create a variable called 'lovesCode' and assign it a boolean value. */
 
 //CODE HERE
-
+console.log("---q3---")
+let lovesCode = true;
 //////////////////PROBLEM 4////////////////////
 /*
   Write an if-statement that checks if the faveNum variable is 
@@ -21,10 +24,19 @@
 */
 
 //CODE HERE
-
+console.log('---q4---')
+if (favNum === 13){
+  console.log('lucky 13!')
+} else {
+  console.log('not lucky 13')
+}
 //////////////////PROBLEM 5////////////////////
 /*
   Write a for loop that prints out your faveNum 5 times.
 */
 
 //CODE HERE
+console.log('---q5---')
+for (let i = 0; i < 5; i++){
+  console.log(favNum)
+}

acc4a334a8eb94515897399829968806961fd3bb
Robert Culbert <bertbert@Roberts-MBP.attlocal.net>
2023-05-22 11:53:32 -0500

    arrays finished

--- arrays.js
+++ arrays.js
@@ -4,11 +4,17 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+console.log("--question 1----")
+colorCopy = faveColors.slice()
+console.log(colorCopy)
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+console.log("--question 2--")
+colorCopy.push('blue')
+console.log(colorCopy)
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,12 +25,15 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
-
+console.log("--question 3--")
+const middleNums = numbers.slice(1, 4)
+console.log(middleNums)
 
 //////////////////PROBLEM 4////////////////////
 /* 
   Create an empty array called 'answers'.
-  Using a for loop, loop over the array called 'bigOrSmallArray', and check to see if the 
+  Using a for loop, loop over the array called 'bigOrSmallArray', 
+  and check to see if the 
   number in the array is GREATER than 100. 
   If it is, push 'big' as a string to the answers array. 
   If the number is LESS than or EQUAL to 100, push 'small' as a string 
@@ -34,3 +43,13 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+console.log("--question 4--")
+const answers = [];
+for (let i = 0; i < bigOrSmallArray.length; i++){
+  if(bigOrSmallArray[i] > 100){
+    answers.push("Big")
+  } else {
+    answers.push("small")
+  }
+}
+console.log(answers)
